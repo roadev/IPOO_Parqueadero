650b2b388815ffcbb23d68e46580bbf6a990a2bf
jdroa92 <jdroa92@gmail.com>
2014-09-22 23:43:32 -0500

    Bug fixes. Arreglos en ciclos.

--- Parqueadero.cpp
+++ Parqueadero.cpp
@@ -20,23 +20,28 @@ Parqueadero::~Parqueadero() {
 
 char Parqueadero::menu(){
     cout<<"Bienvenido al parqueadero"<<endl;
+    char respuestaMenu;
     
     do{
         cout<<"Ingrese la opción 1 para: Entrar carro. \n Opción 2 para: Sacar carro. \n Opcion 3 para: "
                 "Desplegar cupo"<<endl;
-        char respuestaEntrarCarro, respuestaMenu, respuestaSacarCarro;
+        char respuestaEntrarCarro, respuestaSacarCarro;
         
         int opcion;
         cin>>opcion;
         switch(opcion){
             case 1: cout<<"Ha ingresado un vehículo al parqueadero"<<endl;
             carros ++;
+            cuposDisponibles --;
             break;
             case 2: cout<<"Ha retirado un vehículo del parqueadero"<<endl;
             carros --;
+            cuposDisponibles ++;
             break;
             case 3: cout<<"El número de carros en el parqueadero es de: "<<endl;
             return carros;
+            break;
+            default: cout<<"Opción no válida"<<endl;
             break;
         }
         do{
@@ -49,10 +54,12 @@ char Parqueadero::menu(){
         if(cuposDisponibles = 0){
             cout<<"No hay más cupos disponibles, debe desplegar cupos."<<endl;
         }
+        cout<<"¿Desea continuar? \n Para Sí Escriba: s , para No Escriba: n"<<endl;
+        cin>>respuestaMenu;
+        
     }
-    while ((cuposDisponibles <= 20)&& (cuposDisponibles >0));
-
-    
-    
+    while ((respuestaMenu == 's') || (respuestaMenu == 'S'));
+        
+    return 0;
 }
 
